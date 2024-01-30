e58f3306c491ea5fdb05c39574bd08acbe8ffe40
rockgard3n <liamcashel@gmail.com>
2024-01-30 15:44:03 -0800

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Liam Cashel on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
